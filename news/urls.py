647e507e2bf63c7133dae3acf75e714a886c254c
Pietro Bucker <pietro_177@hotmail.com>
2023-09-26 15:41:30 -0300

    req 5

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from news.views import index
+from news.views import details, index
 
 
-urlpatterns = [path("", index, name="home-page")]
+urlpatterns = [
+    path("", index, name="home-page"),
+    path("news/<int:news_id>", details, name='news-details-page')
+    ]
--- news/views.py
+++ news/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from news.models.news_model import News
 
 # Create your views here.
 
 
 def index(request):
-    news = {'news': News.objects.all()}
+    news = {"news": News.objects.all()}
     return render(request, "home.html", news)
+
+
+def details(request, news_id):
+    try:
+        new = get_object_or_404(News, id=news_id)
+        return render(request, "news_details.html", {"new": new})
+    except Http404:
+        return render(request, "404.html")
